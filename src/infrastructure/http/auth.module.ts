dc47b72593e696df4b41bcc1005be6822c014110
tomygp97 <tomasgp1997@gmail.com>
2026-07-07 11:57:39 -0300

    Add JWT authentication support by updating the auth module and adding JWT_SECRET to the environment configuration

--- src/infrastructure/http/auth.module.ts
+++ src/infrastructure/http/auth.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './controllers/auth.controller';
 import { UserPrismaRepository } from '../database/repositories/user.prisma.repository';
 import { DatabaseModule } from '../database/database.module';
 import { UserRepository } from '../../domain/ports/user.repository';
-import { RegisterUserUseCase } from 'src/application/auth/register-user.use-case';
+import { RegisterUserUseCase } from '../../application/auth/register-user.use-case';
+import { LoginUserUseCase } from '../../application/auth/login-user.use-case';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [
     RegisterUserUseCase,
+    LoginUserUseCase,
     {
       provide: UserRepository,
       useClass: UserPrismaRepository,
